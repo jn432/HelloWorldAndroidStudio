57b3495db011f35a12989837bfeca525942ca013
jn432 <jn432@uowmail.edu.au>
2021-08-24 23:02:29 +1000

    Finished first app - button sends to DisplayActivity with message read from text space

--- app/src/main/java/com/example/helloworldandroidstudio/MainActivity.kt
+++ app/src/main/java/com/example/helloworldandroidstudio/MainActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
 
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
 class MainActivity : AppCompatActivity() {
+
+    /** Runs when object is created */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
